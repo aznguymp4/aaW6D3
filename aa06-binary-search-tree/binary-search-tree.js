eca125b6b110ed5f55cf34ad355077d922f7da6b
Erich <erichnguyen25@gmail.com>
2023-10-11 18:26:48 -0500

    asdasdsadfasdf

--- aa06-binary-search-tree/binary-search-tree.js
+++ aa06-binary-search-tree/binary-search-tree.js
@@ -2,3 +2,103 @@
 // `binary-search-tree.js` into this file
 
 // Your code here 
+
+
+
+
+
+
+
+
+
+
+// Do not change this
+class TreeNode {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+
+  constructor() {
+    // Your code here 
+    this.root = null
+  }
+
+  insert(val, currentNode=this.root) {
+    // Your code here 
+    if(!currentNode) return this.root = new TreeNode(val)
+
+    const dir = val < currentNode.val? 'left' : 'right'
+    if(currentNode[dir]) this.insert(val, currentNode[dir])
+    else currentNode[dir] = new TreeNode(val)
+  }
+
+  search(val) {
+    // Your code here
+    if(val===this.root.val) return true
+    
+    let node = this.root
+    while(node) {
+      node = node[val < node.val? 'left' : 'right']
+      if(node?.val === val) return true
+    }
+    return false
+  }
+
+
+  preOrderTraversal(currentNode = this.root) {
+    // Your code here 
+    if(!currentNode) return
+    console.log(currentNode.val);
+    ['left','right'].map(d => this.preOrderTraversal(currentNode[d]))
+  }
+  
+  
+  inOrderTraversal(currentNode = this.root) {
+    // Your code here 
+    if(!currentNode) return
+    console.log((this.inOrderTraversal(currentNode.left) || currentNode).val)
+    this.inOrderTraversal(currentNode.right)
+  }
+  
+  
+  postOrderTraversal(currentNode = this.root) {
+    // Your code here 
+    if(!currentNode) return
+    ['left','right'].map(d => this.postOrderTraversal(currentNode[d]))
+    console.log(currentNode.val)
+  }
+  
+  // Breadth First Traversal - Iterative
+  breadthFirstTraversal() {
+    // Your code here 
+    let q = [this.root]
+
+    while(q.length) {
+      let first = q.shift()
+      console.log(first.val);
+      ['left','right'].map(d => {
+        if(first[d]) q.push(first[d])
+      })
+    }
+  }
+
+  // Depth First Traversal - Iterative
+  depthFirstTraversal() {
+    let s = [this.root]
+
+    while(s.length) {
+      let first = s.pop()
+      console.log(first.val);
+      ['left','right'].map(d => {
+        if(first[d]) s.push(first[d])
+      })
+    }
+  }
+}
+
+module.exports = { BinarySearchTree, TreeNode };
--- aa06-binary-search-tree/tree-practice.js
+++ aa06-binary-search-tree/tree-practice.js
@@ -6,38 +6,133 @@ const { BinarySearchTree, TreeNode } = require('./binary-search-tree.js');
 
 function findMinBST (rootNode) {
   // Your code here 
+  let leftMost = rootNode
+  while(leftMost.left) leftMost = leftMost.left
+  return leftMost.val
 }
 
 function findMaxBST (rootNode) {
   // Your code here 
+  let rightMost = rootNode
+  while(rightMost.right) rightMost = rightMost.right
+  return rightMost.val
 }
 
 function findMinBT (rootNode) {
-  // Your code here 
+  // Your code here
+  let lowest = rootNode.val
+  let q = [rootNode]
+
+  while(q.length) {
+    let first = q.pop()
+    lowest = Math.min(lowest, first.val);
+    ['left','right'].map(d => {
+      if(first[d]) q.push(first[d])
+    })
+  }
+
+  return lowest
 }
 
 function findMaxBT (rootNode) {
   // Your code here 
+  let highest = rootNode.val
+  let q = [rootNode]
+  
+  while(q.length) {
+    let first = q.pop()
+    highest = Math.max(highest, first.val);
+    ['left','right'].map(d => {
+      if(first[d]) q.push(first[d])
+    })
+  }
+  
+  return highest
 }
 
 function getHeight (rootNode) {
   // Your code here 
+  if(!rootNode) return -1
+  if(!(rootNode.left || rootNode.right)) return 0;
+
+  return Math.max(...['left','right'].map((d,i,a) => {
+    let height = 0
+    let idk = rootNode[d]
+    while(idk) {
+      idk = idk[a[i%2]] || idk[a[(i+1)%2]]
+      height++
+    }
+    return height
+  }))
 }
 
 function balancedTree (rootNode) {
   // Your code here 
+  if(!rootNode) return false
+  if(!(rootNode.left || rootNode.right)) return false;
+  
+  const [leftHeight, rightHeight] = ['left','right'].map((d,i,a) => {
+    let height = 0
+    let idk = rootNode[d]
+    while(idk) {
+      idk = idk[a[i%2]] || idk[a[(i+1)%2]]
+      height++
+    }
+    return height
+  })
+
+  return Math.abs(leftHeight-rightHeight) <= 1
 }
 
 function countNodes (rootNode) {
   // Your code here 
+  let count = 0
+  let q = [rootNode]
+  
+  while(q.length) {
+    let first = q.pop()
+    count++
+    ['left','right'].map(d => {
+      if(first[d]) q.push(first[d])
+    })
+  }
+  
+  return count
 }
 
 function getParentNode (rootNode, target) {
   // Your code here 
+  if(rootNode.val === target) return null
+  let lastNode = null
+  let q = [rootNode]
+  
+  while(q.length) {
+    let first = q.pop()
+    if(first.val === target) return lastNode;
+    ['left','right'].map(d => {
+      if(first[d]) {
+        q.push(first[d])
+        lastNode = first
+      }
+    })
+  }
+  return undefined
 }
 
 function inOrderPredecessor (rootNode, target) {
-  // Your code here 
+  // Your code here
+  const targetStr = target.toString()
+  let prev = null
+  let map = {}
+  const pushToArrSorted = (currentNode) => {
+    if(!currentNode || map[targetStr]) return
+    const key = (pushToArrSorted(currentNode.left) || currentNode).val
+    map[key] = prev
+    prev = key
+    pushToArrSorted(currentNode.right)
+  }
+  pushToArrSorted(rootNode)
+  return map[targetStr]
 }
 
 function deleteNodeBST(rootNode, target) {
@@ -46,12 +141,38 @@ function deleteNodeBST(rootNode, target) {
   // Undefined if the target cannot be found
 
   // Set target based on parent
+  let parentNode
+  let currentNode
+
+  if(target==rootNode.val) {
+    currentNode = rootNode
+  } else {
+    let node = rootNode
+    while(node) {
+      let oldNode = node
+      node = node[target < node.val? 'left' : 'right']
+      if(node?.val === target) {
+        parentNode = oldNode
+        currentNode = node
+      }
+      // console.log('======================= ',currentNode.val, parentNode.val)
+    }
+    if(!currentNode) return undefined
+  }
+
+  // console.log('======================= ', currentNode.val, parentNode.val)
 
   // Case 0: Zero children and no parent:
-  //   return null
+  if(rootNode.val === target) {
+    //   return null
+    return null
+  }
 
   // Case 1: Zero children:
-  //   Set the parent that points to it to null
+  if(!(currentNode.left || currentNode.right)) {
+    //   Set the parent that points to it to null
+    parentNode[target < parentNode.val? 'left' : 'right'] = null
+  }
 
   // Case 2: Two children:
   //  Set the value to its in-order predecessor, then delete the predecessor
@@ -60,6 +181,26 @@ function deleteNodeBST(rootNode, target) {
   //  Then delete the child that it was replaced with.
 
   // Case 3: One child:
+  if((currentNode.left && !currentNode.right) || (!currentNode.left && currentNode.right)) {
+    // const actualCurrent = parentNode[target < parentNode.val? 'left' : 'right']
+    // const replacement = actualCurrent[target < actualCurrent.val? 'left' : 'right']
+    // console.log(actualCurrent.val, target, parentNode.val, replacement.val)
+
+    // actualCurrent.val = replacement.val
+    // actualCurrent.left = replacement.left
+    // actualCurrent.right = replacement.right
+  
+    console.log('====================================================')
+    console.log(parentNode)
+    
+    console.log('====================================================')
+    console.log(parentNode[target < parentNode.val? 'left' : 'right'].val)
+    console.log('====================================================', currentNode.val)
+    parentNode[target < parentNode.val? 'left' : 'right'].val = currentNode.val
+    
+    console.log(parentNode)
+    console.log('====================================================')
+  }
   //   Make the parent point to the child
 
 }
